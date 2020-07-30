09ddb2d80d84f460df210a5ea5a1d83b694a0c1e
Tomoyuki HIRAI <gotetu@gmail.com>
2020-07-30 23:12:03 +0900

    動的グラフデータの表示.

--- src/app/strava/bar-chart/bar-chart.component.ts
+++ src/app/strava/bar-chart/bar-chart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {StravaService} from '../services/strava.service';
+import {Color} from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
@@ -8,13 +9,15 @@ import {StravaService} from '../services/strava.service';
 })
 export class BarChartComponent implements OnInit {
   public chartType = 'bar';
-
   public chartOptions = { };
-
   public chartLabels = [];
   public chartLegend = true;
-
   public chartData = [];
+  public colorData = [
+    {backgroundColor: 'rgb(222, 22, 22)'},
+    {backgroundColor: 'rgb(222, 22, 22)'},
+    {backgroundColor: 'rgb(222, 22, 22)'}
+    ];
 
   constructor(private strava: StravaService) { }
 
@@ -25,9 +28,10 @@ export class BarChartComponent implements OnInit {
       for (const activity of activities) {
         if (activity.type === 'Run') {
           data.push(activity.average_speed);
+          this.chartLabels.push(activity.start_date_local);
         }
       }
-      this.chartData.push({data, label});
+      this.chartData.push({ data, label });
     });
   }
 
