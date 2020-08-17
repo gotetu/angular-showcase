44da36613ad0507dd085f0a4cb483e1ef2f46d27
Tomoyuki HIRAI <gotetu@gmail.com>
2020-08-17 23:49:52 +0900

    時刻表記機能の関数化.

--- src/app/strava/pipe/speed-conversion.pipe.ts
+++ src/app/strava/pipe/speed-conversion.pipe.ts
@@ -3,11 +3,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'speed'})
 export class SpeedConversionPipe implements PipeTransform {
     transform(value: number): string {
-        if (value === 0) {
-            return '0:00';
-        }
-        let minuts = Math.floor(1 / ((value * 60) / 1000));
-        let seconds = (1 / ((value * 60) / 1000)) - minuts;
-        return minuts + ':' + ('00' + (60 * seconds).toFixed(0)).slice(-2);
+      return formatSpeed(value);
     }
+}
+export function formatSpeed(value: number): string {
+  if (value === 0) {
+    return '0:00';
+  }
+  const minuts = Math.floor(1 / ((value * 60) / 1000));
+  const seconds = (1 / ((value * 60) / 1000)) - minuts;
+  return minuts + ':' + ('00' + (60 * seconds).toFixed(0)).slice(-2);
 }
